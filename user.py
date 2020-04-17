fa31eff78a25423edb02db147c5abc12ae9d872d
NgugiMuthoni <ngugimuthoni43@gmail.com>
2020-04-17 20:51:12 +0300

    userTest.py added

--- user.py
+++ user.py
@@ -4,35 +4,42 @@ import pyperclip
 class User:
     user_list = []
 
-    def __init__(self, firstName, lastName, userName, password):
+    def __init__(self, first_name, last_name, user_name, password):
 
-        self.firstName = firstName
-        self.lastName = lastName
-        self.userName = userName
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
         self.password = password
 
+    user_list = []
+
     def save_user(self):
+
         User.user_list.append(self)
 
     def delete_user(self):
+
         User.user_list.remove(self)
 
     @classmethod
-    def find_user_by_name(cls, userName):
+    def find_by_user_name(cls, user_name):
+
         for user in cls.user_list:
-            if user.userName == userName:
-                return userName
+            if user.user_name == user_name:
+                return user_name
 
     @classmethod
-    def user_exist(cls, userName):
+    def user_exist(cls, user_name):
+
         for user in cls.user_list:
-            if user.userName == userName:
+            if user.user_name == user_name:
                 return True
 
         return False
 
     @classmethod
     def display_user_name(cls):
+
         return cls.user_list
 
     @classmethod
